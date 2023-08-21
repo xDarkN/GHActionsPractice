8d0cfe51028860d7e97dce15981ae54bdc8b1336
xDarkN <nadavedri1372@gmail.com>
2023-08-21 13:13:09 +0300

    added Workflow

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+describe('Sample Test Suite', () => {
+  it('should add numbers correctly', () => {
+    const sum = 2 + 3;
+    assert.strictEqual(sum, 5);
+  });
+
+  it('should multiply numbers correctly', () => {
+    const product = 4 * 6;
+    assert.strictEqual(product, 24);
+  });
+
+  // Add more test cases as needed
+});
+
