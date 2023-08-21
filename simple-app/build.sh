7cdfc526118cdefbcbe6c146b7fa72da2a54d36b
xDarkN <nadavedri1372@gmail.com>
2023-08-21 14:31:36 +0300

    added Workflow

---
+++ simple-app/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Building the application..."
+# Add your actual build commands here
+# For example, if you're using a Python app, you might have:
+# python setup.py build
---
+++ simple-app/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Running tests..."
+# Add your actual test commands here
+# For example, if you're using a Python app, you might have:
+# python setup.py test
